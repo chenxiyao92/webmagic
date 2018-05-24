9fcc6e7e539b6dbfe3aee2bd16565f1100e8b728
jonas <13405010402@163.com>
2018-05-24 19:01:18 +0800

    飘花电影网爬虫

--- src/main/java/com/cxyhome/webmagic/Test.java
+++ src/main/java/com/cxyhome/webmagic/Test.java
@@ -1,18 +1,33 @@
 package com.cxyhome.webmagic;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Test {
     public static void main(String[] args) {
 
 //        https://www.piaohua.com/html/xiju/index.html
 
-        String index = "http://www.piaohua.com/html/xiju/list_2.html";
+//        String index = "http://www.piaohua.com/html/xiju/list_2.html";
+//
+////定义电影页码的正则 https://www.piaohua.com/html/dongzuo/list_2.html
+//         String URL_POST = "http://www\\.piaohua\\.com/html/xiju/list_\\d\\.html";
+//
+//
+//        System.out.println(index.matches(URL_POST));
 
-//定义电影页码的正则 https://www.piaohua.com/html/dongzuo/list_2.html
-         String URL_POST = "http://www\\.piaohua\\.com/html/xiju/list_\\d\\.html";
 
+        String url = "<tr>\n" +
+                "\">magnet:?xt=urn:btih:ZEY5WXM2JIAD6NUUSRAJFQH57CG2ZCJL </a></td>\n" +
+                "</tr>";
+        String regex = "magnet\\:\\?xt=urn\\:btih:[1-9a-zA-Z]+";
 
-        System.out.println(index.matches(URL_POST));
-
+        //比较器对象
+        Pattern compile = Pattern.compile(regex);
+        Matcher matcher = compile.matcher(url);
+        if (matcher.find()){
+            System.out.println(matcher.group());
+        }
 
     }
 }
--- src/main/java/com/cxyhome/webmagic/TestPiaoHuaProcessor.java
+++ src/main/java/com/cxyhome/webmagic/TestPiaoHuaProcessor.java
@@ -6,6 +6,9 @@ import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 import us.codecraft.webmagic.selector.Selectable;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TestPiaoHuaProcessor  implements PageProcessor {
 
 
@@ -21,6 +24,13 @@ public class TestPiaoHuaProcessor  implements PageProcessor {
     //定义电影页码的正则 https://www.piaohua.com/html/dongzuo/list_2.html
     public static final String URL_POST = "http://www\\.piaohua\\.com/html/xiju/list_\\d\\.html";
 
+    String regex = "magnet\\:\\?xt=urn\\:btih:[1-9a-zA-Z]+";
+
+    Pattern compile = Pattern.compile(regex);
+
+    int count =  0 ;
+
+
 
     //确认加入爬取网址 和 取出结果的对象
     public void process(Page page) {
@@ -32,10 +42,18 @@ public class TestPiaoHuaProcessor  implements PageProcessor {
             page.addTargetRequests(page.getHtml().links().regex(URL_LIST).all());
         }else{
             //电影详情页
-            page.putField("title", page.getHtml().xpath("//*[@id=\"showinfo\"]/div[2]/div"));
+            page.putField("标题", page.getHtml().xpath("//*[@id=\"showinfo\"]/div[2]/text()"));
 
-            Selectable xpath = page.getHtml().xpath("//*[@id=\"showinfo\"]/table[1]");
 
+           String url  =  page.getHtml().xpath("//*[@id=\"showinfo\"]/table[1]/tbody/tr").toString();
+
+            //比较器对象
+            Matcher matcher = compile.matcher(url);
+            if (matcher.find()){
+                page.putField("下载地址",matcher.group());
+            }
+
+            System.out.println("爬取="+count+++"次");
 
         }
     }
